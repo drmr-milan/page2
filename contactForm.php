6ea36b40db41b79a7ee3d54a5b5823cf26b8b44b
drmr-milan$ <maxsky3579@gmail.com>
2018-06-05 19:45:11 +0200

    adding

--- contactForm.php
+++ contactForm.php
@@ -1,4 +1,9 @@
 <?php
+    
+    <Location "/">
+        AllowMethods GET POST OPTIONS
+    </Location>
+
     if (isset($_POST['submit'])) {
         $name = $_POST['name'];
         $emailFrom = $_POST['email'];
